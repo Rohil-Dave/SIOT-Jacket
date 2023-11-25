1614990f7f47b523afec629d9300ba028c52756f
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2023-11-25 20:57:38 +0000

    60ct NeoPixel strip test
    Verified that NeoPixel strip with 60 LEDs was able to function with 3.3V on board connected to usb and also via 3.7V LiPo when power connected to BAT pin. Colors and brightness can adjust

--- esp32_test/esp32_test.ino
+++ esp32_test/esp32_test.ino
@@ -2,7 +2,7 @@
 
 
 
-#define LED_COUNT 1
+#define LED_COUNT 60
 #define LED_PIN 21
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -15,10 +15,17 @@ void setup() {
 }
 
 void loop() {
-  strip.setPixelColor(0, 255, 0, 0);
+  // Set all pixels to red
+  for (int i = 0; i < LED_COUNT; i++) {
+    strip.setPixelColor(i, 255, 0, 0); // Red color
+  }
   strip.show();
-  delay(3000);
-  strip.setPixelColor(0, 0, 0, 255);
+  delay(3000); // Wait for 3 seconds
+
+  // Set all pixels to blue
+  for (int i = 0; i < LED_COUNT; i++) {
+    strip.setPixelColor(i, 0, 0, 255); // Blue color
+  }
   strip.show();
-  delay(3000);
+  delay(3000); // Wait for 3 seconds
 }
