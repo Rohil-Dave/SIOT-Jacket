0ff5a2698a86a9a8fe556643e11cf4215a0e1dc6
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2023-11-25 18:43:58 +0000

    Vibe disc test
    Verified that vibration mini motor discs work. Can use resistors to have a less intense buzz

--- esp32_test/esp32_test.ino
+++ esp32_test/esp32_test.ino
@@ -1,15 +1,15 @@
 
-const int LED_OUTPUT_PIN = 21;
+const int MOTOR_OUTPUT_PIN = 21;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_OUTPUT_PIN, OUTPUT);
+  pinMode(MOTOR_OUTPUT_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_OUTPUT_PIN, HIGH);   // turn LED on (3.3V)
-  delay(1000);                          // delay is in milliseconds; so wait one second
-  digitalWrite(LED_OUTPUT_PIN, LOW);    // turn LED off (0V)
-  delay(1000);                          // wait for a second
+  digitalWrite(MOTOR_OUTPUT_PIN, HIGH);   // turn LED on (3.3V)
+  delay(2000);                          // delay is in milliseconds; so wait one second
+  digitalWrite(MOTOR_OUTPUT_PIN, LOW);    // turn LED off (0V)
+  delay(2000);                          // wait for a second
 }
