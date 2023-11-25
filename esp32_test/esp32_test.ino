5dce6d1795cbe8e9c7523efd7bcba39110ee5cc3
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2023-11-25 17:48:22 +0000

    Blink test
    Verified that esp32 is working. Simple red led blink test works both on usb plug in and on LiPo plug in.

--- esp32_test/esp32_test.ino
+++ esp32_test/esp32_test.ino
@@ -1,9 +1,15 @@
+
+const int LED_OUTPUT_PIN = 21;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_OUTPUT_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_OUTPUT_PIN, HIGH);   // turn LED on (3.3V)
+  delay(1000);                          // delay is in milliseconds; so wait one second
+  digitalWrite(LED_OUTPUT_PIN, LOW);    // turn LED off (0V)
+  delay(1000);                          // wait for a second
 }
