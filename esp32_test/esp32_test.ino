c7cd1ecb331edc574d9c5e5e2bd2f24a393566d5
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2023-12-02 18:55:26 +0000

    MMA x2 control NeoPixel Strips respectively
    Now connected two different accelerometers which each separate control the activation of respective leds on sleeve

--- esp32_test/esp32_test.ino
+++ esp32_test/esp32_test.ino
@@ -4,105 +4,94 @@
 #include <Adafruit_MMA8451.h>
 
 #define LED_COUNT 60
-#define LED_PIN 21
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define RIGHT_LED_PIN 21
+#define LEFT_LED_PIN 17
 
-Adafruit_MMA8451 mma = Adafruit_MMA8451();
+#define RIGHT_VIBE_PIN 34
+#define LEFT_VIBE_PIN 25
 
-float prevXacc = 0; // Variable to store previous X acceleration
-float threshold = 2.0; // Variable to check against for change in X acceleration to trigger light change
+Adafruit_NeoPixel rightSleeveStrip(LED_COUNT, RIGHT_LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leftSleeveStrip(LED_COUNT, LEFT_LED_PIN, NEO_GRB + NEO_KHZ800);
+
+Adafruit_MMA8451 rightSleeveMMA = Adafruit_MMA8451();
+Adafruit_MMA8451 leftSleeveMMA = Adafruit_MMA8451();
+
+float prevRightXacc = 0; // Variable to store previous X acceleration of MMA on right sleeve
+float prevLeftXacc = 0; // Variable to store previous X acceleration of MMA on left sleeve
+float threshold = 4.0; // Variable to check against for change in X acceleration to trigger light change
 
 
 void setup(void) {
   Serial.begin(9600);
   Serial.println("Adafruit MMA8451 test!");
   
-  if (! mma.begin()) {
-    Serial.println("Couldnt start");
+  // Initialize Right Sleeve MMA8451
+  if (! rightSleeveMMA.begin(0x1D)) { // Unique I2C address for the right sleeve accelerometer
+    Serial.println("Couldn't start right sleeve MMA8451");
     while (1);
   }
-  Serial.println("MMA8451 found!");
-  
-  mma.setRange(MMA8451_RANGE_2_G);
-  
-  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
-  Serial.println("G");
+  Serial.println("Right sleeve MMA8451 found!");
 
-  strip.begin(); // Initialize NeoPixel LED
-  strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(100);  // range: 0 to 255
+  // Initialize Left Sleeve MMA8451
+  if (! leftSleeveMMA.begin(0x1C)) { // Unique I2C address for the left sleeve accelerometer, 0x1C requires A pin to GND
+    Serial.println("Couldn't start left sleeve MMA8451");
+    while (1);
+  }
+  Serial.println("Left sleeve MMA8451 found!");
+  
+  rightSleeveMMA.setRange(MMA8451_RANGE_2_G);
+  leftSleeveMMA.setRange(MMA8451_RANGE_2_G);
+
+  pinMode(RIGHT_VIBE_PIN, OUTPUT);
+  pinMode(LEFT_VIBE_PIN, OUTPUT);
+
+  rightSleeveStrip.begin(); // Initialize Right NeoPixel LED strip
+  rightSleeveStrip.show(); // Initialize all pixels to 'off'
+  rightSleeveStrip.setBrightness(64);  // range: 0 to 255
+
+  leftSleeveStrip.begin(); // Initialize Left NeoPixel LED strip
+  leftSleeveStrip.show(); // Initialize all pixels to 'off'
+  leftSleeveStrip.setBrightness(64);  // range: 0 to 255
 }
 
-void loop() {
-  // Read the 'raw' data in 14-bit counts
+// Function that checks respective change in acceleration to activate LEDs and vibe motor on right or left sleeve
+void processSleeve(Adafruit_MMA8451 &mma, Adafruit_NeoPixel &strip, float &prevXacc, const char* sleeveName, int motorPin) {
   mma.read();
-  Serial.print("X:\t"); Serial.print(mma.x); 
-  Serial.print("\tY:\t"); Serial.print(mma.y); 
-  Serial.print("\tZ:\t"); Serial.print(mma.z); 
-  Serial.println();
-
-  /* Get a new sensor event */ 
   sensors_event_t event; 
   mma.getEvent(&event);
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: \t"); Serial.print(event.acceleration.x); Serial.print("\t");
-  Serial.print("Y: \t"); Serial.print(event.acceleration.y); Serial.print("\t");
-  Serial.print("Z: \t"); Serial.print(event.acceleration.z); Serial.print("\t");
-  Serial.println("m/s^2 ");
+  // Printing the accelerometer data for the specific sleeve
+  Serial.print(sleeveName);
+  Serial.print(" X:\t"); Serial.print(event.acceleration.x); Serial.print("\t");
+  Serial.print("Y:\t"); Serial.print(event.acceleration.y); Serial.print("\t");
+  Serial.print("Z:\t"); Serial.print(event.acceleration.z); Serial.println(" m/s^2");
 
-  // Get the current acceleration
   float currentXacc = event.acceleration.x;
 
-  // Check for a change in acceleration;
   if (abs(currentXacc - prevXacc) > threshold) {
-    // Turn on the Neopixel
     for (int i = 0; i < LED_COUNT; i++) {
-    strip.setPixelColor(i, 200, 200, 200); // White-ish color
+      strip.setPixelColor(i, 225, 0, 0); // White-ish color
     }
-    strip.show();
+    strip.show();  // Turn on LEDs
+    digitalWrite(motorPin, HIGH); // Turn on vibe motor
     delay(3000);
+    digitalWrite(motorPin, LOW); // Turn off vibe motor
   } else {
-    //Turn off the NeoPixel
     for (int i = 0; i < LED_COUNT; i++) {
-    strip.setPixelColor(i, 0, 0, 0); // Off
+      strip.setPixelColor(i, 0, 0, 0); // Off
     }
     strip.show();
   }
   
   prevXacc = currentXacc;
-  
-  
-  /* Get the orientation of the sensor */
-  uint8_t o = mma.getOrientation();
-  
-  switch (o) {
-    case MMA8451_PL_PUF: 
-      Serial.println("Portrait Up Front");
-      break;
-    case MMA8451_PL_PUB: 
-      Serial.println("Portrait Up Back");
-      break;    
-    case MMA8451_PL_PDF: 
-      Serial.println("Portrait Down Front");
-      break;
-    case MMA8451_PL_PDB: 
-      Serial.println("Portrait Down Back");
-      break;
-    case MMA8451_PL_LRF: 
-      Serial.println("Landscape Right Front");
-      break;
-    case MMA8451_PL_LRB: 
-      Serial.println("Landscape Right Back");
-      break;
-    case MMA8451_PL_LLF: 
-      Serial.println("Landscape Left Front");
-      break;
-    case MMA8451_PL_LLB: 
-      Serial.println("Landscape Left Back");
-      break;
-    }
-  Serial.println();
-  delay(500);
-  
+}
+
+void loop() {
+  // Process right sleeve and print its data
+  processSleeve(rightSleeveMMA, rightSleeveStrip, prevRightXacc, "Right Sleeve", RIGHT_VIBE_PIN);
+
+  // Process left sleeve and print its data
+  processSleeve(leftSleeveMMA, leftSleeveStrip, prevLeftXacc, "Left Sleeve", LEFT_VIBE_PIN);
+
+  delay(1000);
 }
